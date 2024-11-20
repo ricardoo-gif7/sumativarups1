0da66b5dc7564b40e2554fe09166e74d9578f761
ricardouzhca26 <ricardouzhca26@gmail.com>
2024-11-20 01:28:48 +0000

    first commit

--- src/app/Components/output-view/output-view.component.ts
+++ src/app/Components/output-view/output-view.component.ts
@@ -10,12 +10,5 @@ export class OutputViewComponent {
   @Input() email: string = '';
   @Input() password: string = '';
   @Input() text: string = '';
-  animate: boolean = false;
 
-  onAnimate() {
-    this.animate = true;
-    setTimeout(() => {
-      this.animate = false; // Para quitar la animación después de un tiempo
-    }, 500); // Duración de la animación
-  }
 }
